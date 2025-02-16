c89dbd7b0a910d1bfdfeafc9562b969cd37012c5
Elimu Jonathan <elimu.jonathan@stud.umu.ac.ug>
2025-02-16 12:02:21 -0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+function changeBackgroundColor() {
+    const colors = ['lightblue', 'lightgreen', 'lightcoral', 'lightyellow', 'lightpink'];
+    document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+}
+</script>
